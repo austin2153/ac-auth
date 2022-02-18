a8861cc41fa651a01af0e75837895a560f06b554
Austin Campbell <austincamp@gmail.com>
2022-02-18 16:33:13 -0500

    updates...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   firebase: {
     projectId: 'ac-auth-firebase',
     appId: '1:135706770618:web:c975d46eb11b0c6d335683',
+    databaseURL: 'https://ac-auth-firebase-default-rtdb.firebaseio.com',
     storageBucket: 'ac-auth-firebase.appspot.com',
     apiKey: 'AIzaSyAcZtzR66yBdG7qYRFj9T3JSf9W2dzP6dE',
     authDomain: 'ac-auth-firebase.firebaseapp.com',
